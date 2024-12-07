6279b02ae77146c5f75665ee176283cf0984c5ba
silvercatcloud <149999521+silvercatcloud@users.noreply.github.com>
2024-12-07 17:04:23 +0800

    文章分类界面

---
+++ src/components/PageContainer.vue
@@ -0,0 +1,39 @@
+<!--考虑到多个页面复用，封装成组件
+  - props 定制标题
+  - 默认插槽 default 定制内容主体
+  - 具名插槽 extra  定制头部右侧额外的按钮
+-->
+<script setup>
+defineProps({
+  title: {
+    required: true,
+    type: String,
+  },
+})
+</script>
+
+<template>
+  <el-card class="page-container">
+    <template #header>
+      <div class="header">
+        <span>{{ title }}</span>
+        <div class="extra">
+          <slot name="extra"></slot>
+        </div>
+      </div>
+    </template>
+    <slot></slot>
+  </el-card>
+</template>
+
+<style lang="scss" scoped>
+.page-container {
+  min-height: 100%;
+  box-sizing: border-box;
+  .header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+}
+</style>
---
+++ src/views/article/components/ChannelEdit.vue
@@ -0,0 +1,80 @@
+<!--添加 和 编辑，可以共用一个弹层，所以可以将弹层封装成一个组件-->
+<script setup>
+import { ref } from 'vue'
+import { ElMessage } from 'element-plus'
+import { articleCategoryAddService, articleCategoryUpdateService } from '@/api/article.js'
+const dialogVisible = ref(false)
+
+const fromModel = ref({
+  categoryName: '',
+  categoryAlias: '',
+})
+const rules = {
+  categoryName: [
+    { required: true, message: '请输入分类名称', trigger: 'blur' },
+    {
+      pattern: /^\S{1,10}$/,
+      message: '分类名必须是1-10位的非空字符',
+      trigger: 'blur',
+    },
+  ],
+  categoryAlias: [
+    { required: true, message: '请输入分类别名', trigger: 'blur' },
+    {
+      pattern: /^[a-zA-Z0-9]{1,15}$/,
+      message: '分类别名必须是1-15位的字母数字',
+      trigger: 'blur',
+    },
+  ],
+}
+const fromRef = ref()
+const emit = defineEmits(['success'])
+const onSubmit = async () => {
+  console.log(fromModel.value)
+  await fromRef.value.validate()
+  fromModel.value.id //通过 id（存在，修改，不存在，添加 ）判断
+    ? await articleCategoryUpdateService(fromModel.value)
+    : await articleCategoryAddService(fromModel.value)
+  ElMessage({
+    type: 'success',
+    message: fromModel.value.id ? '编辑成功' : '添加成功',
+  })
+  dialogVisible.value = false
+  emit('success')
+}
+const open = (row) => {
+  console.log(row)
+  dialogVisible.value = true
+  fromModel.value = { ...row }
+}
+defineExpose({
+  open,
+})
+</script>
+
+<template>
+  <el-dialog v-model="dialogVisible" :title="fromModel.id ? '修改分类' : '添加分类'" width="30%">
+    <el-form
+      :model="fromModel"
+      :rules="rules"
+      ref="fromRef"
+      label-width="100px"
+      style="padding-right: 30px"
+    >
+      <el-form-item label="分类名称" prop="categoryName">
+        <el-input v-model="fromModel.categoryName" placeholder="请输入分类名称"></el-input>
+      </el-form-item>
+      <el-form-item label="分类别名" prop="categoryAlias">
+        <el-input v-model="fromModel.categoryAlias" placeholder="请输入分类别名"></el-input>
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="dialogVisible = false">取消</el-button>
+        <el-button type="primary" @click="onSubmit"> 确认 </el-button>
+      </span>
+    </template>
+  </el-dialog>
+</template>
+
+<style scoped></style>
